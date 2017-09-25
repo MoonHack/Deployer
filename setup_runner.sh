d52a26b2190e20349de798e746fad9feef1b93a6
Doridian <mriq91@gmail.com>
2017-09-24 18:49:00 -0700

    stuff

--- setup_runner.sh
+++ setup_runner.sh
@@ -2,6 +2,8 @@
 _CAT=runner
 source shared.sh
 
+setup
+
 run 'apt -y install libssl-dev libluajit-5.1-dev luajit luarocks build-essential cmake librabbitmq-dev uuid-dev'
 
 run 'cd /usr/src && wget https://github.com/mongodb/mongo-c-driver/releases/download/1.8.0/mongo-c-driver-1.8.0.tar.gz -O mongo-c-driver-1.8.0.tar.gz && tar xzf mongo-c-driver-1.8.0.tar.gz && cd mongo-c-driver-1.8.0 && ./configure --disable-automatic-init-and-cleanup --disable-shm-counters && make && make install'
@@ -19,4 +21,4 @@ run 'cd /home/runner/Runner/build && cmake -DCMAKE_BUILD_TYPE=Release .. && make
 scp 'Runner/mh_runner.service' "$LOGIN:/etc/systemd/system/mh_runner.service"
 run 'systemctl daemon-reload && systemctl enable mh_runner && systemctl restart mh_runner'
 
-finish
+finish_setup
--- shared.sh
+++ shared.sh
@@ -8,17 +8,19 @@ HOSTNAME="$_CAT$2.moonhack.net"
 run() {
 	ssh -oForwardAgent=yes "$LOGIN" "$@"
 }
-finish() {
-	run 'echo Done'
-	#run reboot
+finish_setup() {
+	run reboot
 }
 ssh-copy-id "root@$IP"
 
-run 'touch ~/.ssh/known_hosts'
-run 'ssh-keyscan gitlab.com 2>&1 | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts'
-run 'mv ~/.ssh/tmp_hosts ~/.ssh/known_hosts'
+setup() {
+	run 'touch ~/.ssh/known_hosts'
+	run 'ssh-keyscan gitlab.com 2>&1 | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts'
+	run 'mv ~/.ssh/tmp_hosts ~/.ssh/known_hosts'
+
+	run 'apt -y install sudo htop screen tcpdump git'
+	run "hostnamectl set-hostname $HOSTNAME"
+	run 'timedatectl set-timezone UTC'
+}
 
 run 'apt -y update && apt -y upgrade'
-run 'apt -y install sudo htop screen tcpdump git'
-run "hostnamectl set-hostname $HOSTNAME"
-run 'timedatectl set-timezone UTC'
