dad90da812de71866dea1c13368107fea3e77429
Doridian <mriq91@gmail.com>
2017-09-25 15:40:44 -0700

    web

--- setup_web.sh
+++ setup_web.sh
@@ -14,7 +14,7 @@ run 'systemctl enable nginx && systemctl restart nginx'
 run 'useradd web || true'
 run 'cd /opt/Web && git pull' || run 'cd /opt && git clone git@gitlab.com:MoonHack/Web'
 scp 'Web/config.ts' "$LOGIN:/opt/Web/src/config.ts"
-run 'cd /opt/Web && npm run build'
+run 'cd /opt/Web && npm install -s && npm run build -s'
 scp 'Web/mh_web.service' "$LOGIN:/etc/systemd/system/mh_web.service"
 run 'systemctl daemon-reload && systemctl enable mh_web && systemctl restart mh_web'
 
--- update_web.sh
+++ update_web.sh
@@ -4,6 +4,6 @@ source shared.sh
 
 run 'cd /opt/Web && git pull'
 scp 'Web/config.ts' "$LOGIN:/opt/Web/src/config.ts"
-run 'cd /opt/Web && npm run build'
+run 'cd /opt/Web && npm install -s && npm run build -s'
 scp 'Web/mh_web.service' "$LOGIN:/etc/systemd/system/mh_web.service"
 run 'systemctl daemon-reload && systemctl enable mh_web && systemctl restart mh_web'
