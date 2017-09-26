8a91adf163dd5edbf65f0d2978eb3f5f7591da3a
Doridian <mriq91@gmail.com>
2017-09-26 15:56:58 -0700

    dkjson

--- update_runner.sh
+++ update_runner.sh
@@ -8,4 +8,3 @@ scp 'Runner/config.h' "$LOGIN:/opt/Runner/src/c/config.h"
 run 'cd /opt/Runner/build && cmake -DCMAKE_BUILD_TYPE=Release .. && make'
 scp 'Runner/mh_runner.service' "$LOGIN:/etc/systemd/system/mh_runner.service"
 run 'systemctl daemon-reload && systemctl enable mh_runner && systemctl restart mh_runner'
-
